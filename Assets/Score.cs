3fd8ef21389953776c793e0b8b26491c16940af2
Kelly MacNeill <kelly@pyromuffin.com>
2020-02-02 10:16:25 -0800

    added sound, timer, and points

---
+++ Assets/Score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI text;
+    int points;
+
+    public void AddPoint()
+    {
+        points++;
+        text.text = "Cat Pets: " + points.ToString();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = "Cat Pets: " + points.ToString();
+    }
+}
