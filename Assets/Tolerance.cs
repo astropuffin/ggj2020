9d6b274ae71e5b0236d65b0521034f58526be091
NoahZr <n.zucker1@gmail.com>
2020-02-02 16:43:30 -0500

    goal/time tolerence

--- Assets/Tolerance.cs
+++ Assets/Tolerance.cs
@@ -7,17 +7,22 @@ public class Tolerance : MonoBehaviour
     public TMPro.TextMeshProUGUI text;
     public float timePerPoint;
     public float timer;
-
+    public float timerStartVal;
+    public Gradient badToGood;
     public void AddPoint()
     {
         timer += timePerPoint;
     }
-
+    void Start()
+    {
+        timer = timerStartVal;
+    }
 
     // Update is called once per frame
     void Update()
     {
         text.text = "Tolerance Exhausted in: " + timer.ToString("F1");
         timer -= Time.deltaTime;
+        text.color = badToGood.Evaluate(Mathf.Clamp01(timer / timerStartVal));
     }
 }
