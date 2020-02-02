90dd5f706b183c901c7d89b930d0d7143104c5d4
NoahZr <n.zucker1@gmail.com>
2020-02-01 21:19:24 -0500

    Cat assets and prefabs

---
+++ Assets/Materials/Noah stuff/catAnimations.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+
+public class catAnimations : MonoBehaviour
+{
+    public Animator ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKey(KeyCode.B))
+        {
+            ani.SetFloat("howMuchHappy", Mathf.Lerp(ani.GetFloat("howMuchHappy"), 1, 5*Time.deltaTime));
+        } 
+       else
+        {
+            ani.SetFloat("howMuchHappy", Mathf.Lerp(ani.GetFloat("howMuchHappy"), 0, 5 * Time.deltaTime));
+        }
+    }
+}
