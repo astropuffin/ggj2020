9573c54afc78c5ccb36b4d4bba0ecb8c55d71b2d
astropuffin <astropuffin@users.noreply.github.com>
2020-02-01 17:48:36 -0800

    audio manager

---
+++ Assets/Scripts/AudioSynth2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSynth2 : MonoBehaviour
+{
+    private readonly double _samplingFrequency = 44100.0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
